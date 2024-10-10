99f79ccc57b041a55e1d0898f65849383fdd7353
yonatan alebachew <yonatanalebachew7@gmail.com>
2024-10-09 15:22:58 -0900

    bson data type added to the model

--- models/webhook_payload.go
+++ models/webhook_payload.go
@@ -1,13 +1,13 @@
 package models
 
 type WebhookPayload struct {
-	ID          string `json:"id"`
-	Amount      int    `json:"amount"`
-	Currency    string `json:"currency"`
-	CreatedAt   int64  `json:"created_at_time"`
-	Timestamp   int64  `json:"timestamp"`
-	Cause       string `json:"cause"`
-	FullName    string `json:"full_name"`
-	AccountName string `json:"account_name"`
-	InvoiceURL  string `json:"invoice_url"`
+	ID          string `json:"id" bson:"id"`
+	Amount      int    `json:"amount" bson:"amount"`
+	Currency    string `json:"currency" bson:"currency"`
+	CreatedAt   int64  `json:"created_at_time" bson:"created_at_time"`
+	Timestamp   int64  `json:"timestamp" bson:"timestamp"`
+	Cause       string `json:"cause" bson:"cause"`
+	FullName    string `json:"full_name" bson:"full_name"`
+	AccountName string `json:"account_name" bson:"account_name"`
+	InvoiceURL  string `json:"invoice_url" bson:"invoice_url"`
 }
